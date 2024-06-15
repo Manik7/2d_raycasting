13548a750af588b9f53fdf270805cee2dfcf68cb
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-15 17:24:38 +0200

    Revert "modify image to be 3-dimensional, so there is space for separate channels"
    This reverts commit 6df6e0c4d0ac54653ec1010e54ce35297e5cfad3.

--- src/image.hpp
+++ src/image.hpp
@@ -8,22 +8,19 @@ struct Coordinate
 {
     int row{};
     int col{};
-    int cha{};
 };
 
-template <typename T, int Height, int Width, int Depth, int Padding = 0>
+template <typename T, int Height, int Width, int Padding = 0>
 struct Image
 {
-    static constexpr int height()                         { return Height; }
-    static constexpr int width()                          { return Width; }
-    static constexpr int depth()                          { return Depth; }
-    static constexpr int size()                           { return Height * Width * Depth; }
-    static constexpr int padding()                        { return Padding; }
-    static constexpr int padded_height()                  { return Height+2*Padding; }
-    static constexpr int padded_width()                   { return Width+2*Padding; }
-    static constexpr int padded_depth()                   { return Depth+2*Padding; }
-    static constexpr int padded_size()                    { return padded_height() * padded_width() * padded_depth(); }
-    static constexpr int index(int row, int col, int cha) { return (row+Padding)*padded_width()*padded_depth() + (col+Padding)*padded_depth() + cha+Padding; } // row-major
+    static constexpr int height()                { return Height; }
+    static constexpr int width()                 { return Width; }
+    static constexpr int size()                  { return Height * Width; }
+    static constexpr int padding()               { return Padding; }
+    static constexpr int padded_height()         { return Height+2*Padding; }
+    static constexpr int padded_width()          { return Width+2*Padding; }
+    static constexpr int padded_size()           { return padded_height() * padded_width(); }
+    static constexpr int index(int row, int col) { return (row+Padding)*padded_width() + (col+Padding); } // row-major
 
     Image() = default;
     Image(std::array<T, size()> const& contents, T padding_value)
@@ -33,18 +30,15 @@ struct Image
         {
             for (int w = 0; w < Width; ++w)
             {
-                for (int d = 0; d < Depth; ++d)
-                {
-                    (*this)(h, w, d) = contents[h*Width*Depth + w*Depth + d];
-                }
+                (*this)(h, w) = contents[h*Width + w];
             }
         }
     }
     
-    T& operator()(int row, int col, int cha)             { return _data[index(row, col, cha)]; }
-    T& operator()(Coordinate c)                          { return _data[index(c.row, c.col, c.cha)]; }
-    const T& operator()(int row, int col, int cha) const { return _data[index(row, col, cha)]; }
-    const T& operator()(Coordinate c) const              { return _data[index(c.row, c.col, c.cha)]; }
+    T& operator()(int row, int col)              { return _data[index(row, col)]; }
+    T& operator()(Coordinate c)                  { return _data[index(c.row, c.col)]; }
+    const T& operator()(int row, int col) const  { return _data[index(row, col)]; }
+    const T& operator()(Coordinate c) const      { return _data[index(c.row, c.col)]; }
 
     std::array<T, padded_size()> _data{};
 };
--- test/test_image.cpp
+++ test/test_image.cpp
@@ -6,105 +6,92 @@ namespace {
 
 TEST(image, index)
 {
-    const rc::Image<char, 3, 2, 1, 0> image{};
+    const rc::Image<char, 3, 2> image{};
 
-    EXPECT_EQ(image.index(0, 0, 0), 0);
-    EXPECT_EQ(image.index(0, 1, 0), 1);
-    EXPECT_EQ(image.index(1, 0, 0), 2);
-    EXPECT_EQ(image.index(1, 1, 0), 3);
-    EXPECT_EQ(image.index(2, 0, 0), 4);
-    EXPECT_EQ(image.index(2, 1, 0), 5);
+    EXPECT_EQ(image.index(0, 0), 0);
+    EXPECT_EQ(image.index(0, 1), 1);
+    EXPECT_EQ(image.index(1, 0), 2);
+    EXPECT_EQ(image.index(1, 1), 3);
+    EXPECT_EQ(image.index(2, 0), 4);
+    EXPECT_EQ(image.index(2, 1), 5);
 }
 
 TEST(image, index_padded)
 {
     //  .  .  .  .
-    //  . 25 26  .
-    //  . 29 30  .
-    //  . 33 34  .
+    //  .  5  6  .
+    //  .  9 10  .
+    //  . 13 14  .
     //  .  .  .  .
 
-    const rc::Image<char, 3, 2, 1, 1> image{};
+    const rc::Image<char, 3, 2, 1> image{};
 
-    ASSERT_EQ(image.size(), 6);
-    ASSERT_EQ(image.height(), 3);
-    ASSERT_EQ(image.width(), 2);
-    ASSERT_EQ(image.padded_size(), 60);
-    ASSERT_EQ(image.padded_height(), 5);
-    ASSERT_EQ(image.padded_width(), 4);
-    ASSERT_EQ(image.padded_depth(), 3);
-
-    EXPECT_EQ(image.index(0, 0, 0), 16);
-    EXPECT_EQ(image.index(0, 1, 0), 19);
-    EXPECT_EQ(image.index(1, 0, 0), 28);
-    EXPECT_EQ(image.index(1, 1, 0), 31);
-    EXPECT_EQ(image.index(2, 0, 0), 40);
-    EXPECT_EQ(image.index(2, 1, 0), 43);
+    EXPECT_EQ(image.index(0, 0), 5);
+    EXPECT_EQ(image.index(0, 1), 6);
+    EXPECT_EQ(image.index(1, 0), 9);
+    EXPECT_EQ(image.index(1, 1), 10);
+    EXPECT_EQ(image.index(2, 0), 13);
+    EXPECT_EQ(image.index(2, 1), 14);
 }
 
 TEST(image, empty)
 {
-    const rc::Image<char, 0, 0, 0, 0> image{};
+    const rc::Image<char, 0, 0> image{};
 
     ASSERT_EQ(image.size(), 0);
     ASSERT_EQ(image._data.size(), 0);
     EXPECT_EQ(image.height(), 0);
     EXPECT_EQ(image.width(), 0);
-    EXPECT_EQ(image.depth(), 0);
 }
 
 TEST(image, symbols)
 {
-    const rc::Image<char, 1, 6, 1, 0> image({'a', 'b', 'c', 'd', 'e', 'f'}, '0');
+    const rc::Image<char, 1, 6> image({'a', 'b', 'c', 'd', 'e', 'f'}, '0');
 
     ASSERT_EQ(image.size(), 6);
     ASSERT_EQ(image._data.size(), 6);
     EXPECT_EQ(image.height(), 1);
     EXPECT_EQ(image.width(), 6);
-    ASSERT_EQ(image.depth(), 1);
-    EXPECT_EQ(image(0, 0, 0), 'a');
-    EXPECT_EQ(image(0, 1, 0), 'b');
-    EXPECT_EQ(image(0, 2, 0), 'c');
-    EXPECT_EQ(image(0, 3, 0), 'd');
-    EXPECT_EQ(image(0, 4, 0), 'e');
-    EXPECT_EQ(image(0, 5, 0), 'f');
+    EXPECT_EQ(image(0, 0), 'a');
+    EXPECT_EQ(image(0, 1), 'b');
+    EXPECT_EQ(image(0, 2), 'c');
+    EXPECT_EQ(image(0, 3), 'd');
+    EXPECT_EQ(image(0, 4), 'e');
+    EXPECT_EQ(image(0, 5), 'f');
 }
 
 TEST(image, six)
 {
-    const rc::Image<char, 3, 2, 1, 0> image({'a', 'b', 'c', 'd', 'e', 'f'}, '0');
+    const rc::Image<char, 3, 2> image({'a', 'b', 'c', 'd', 'e', 'f'}, '0');
 
     ASSERT_EQ(image.size(), 6);
     ASSERT_EQ(image._data.size(), 6);
     EXPECT_EQ(image.height(), 3);
     EXPECT_EQ(image.width(), 2);
-    ASSERT_EQ(image.depth(), 1);
-    EXPECT_EQ(image(0, 0, 0), 'a');
-    EXPECT_EQ(image(0, 1, 0), 'b');
-    EXPECT_EQ(image(1, 0, 0), 'c');
-    EXPECT_EQ(image(1, 1, 0), 'd');
-    EXPECT_EQ(image(2, 0, 0), 'e');
-    EXPECT_EQ(image(2, 1, 0), 'f');
+    EXPECT_EQ(image(0, 0), 'a');
+    EXPECT_EQ(image(0, 1), 'b');
+    EXPECT_EQ(image(1, 0), 'c');
+    EXPECT_EQ(image(1, 1), 'd');
+    EXPECT_EQ(image(2, 0), 'e');
+    EXPECT_EQ(image(2, 1), 'f');
 }
 
 TEST(image, six_padded)
 {
-    const rc::Image<char, 3, 2, 1, 1> image({'a', 'b', 'c', 'd', 'e', 'f'}, '0');
+    const rc::Image<char, 3, 2, 1> image({'a', 'b', 'c', 'd', 'e', 'f'}, '0');
 
     ASSERT_EQ(image.size(), 6);
     ASSERT_EQ(image.height(), 3);
     ASSERT_EQ(image.width(), 2);
-    ASSERT_EQ(image.depth(), 1);
-    ASSERT_EQ(image.padded_size(), 60);
+    ASSERT_EQ(image.padded_size(), 20);
     ASSERT_EQ(image.padded_height(), 5);
     ASSERT_EQ(image.padded_width(), 4);
-    ASSERT_EQ(image.padded_depth(), 3);
-    EXPECT_EQ(image(0, 0, 0), 'a');
-    EXPECT_EQ(image(0, 1, 0), 'b');
-    EXPECT_EQ(image(1, 0, 0), 'c');
-    EXPECT_EQ(image(1, 1, 0), 'd');
-    EXPECT_EQ(image(2, 0, 0), 'e');
-    EXPECT_EQ(image(2, 1, 0), 'f');
+    EXPECT_EQ(image(0, 0), 'a');
+    EXPECT_EQ(image(0, 1), 'b');
+    EXPECT_EQ(image(1, 0), 'c');
+    EXPECT_EQ(image(1, 1), 'd');
+    EXPECT_EQ(image(2, 0), 'e');
+    EXPECT_EQ(image(2, 1), 'f');
 }
 
 TEST(image, floats)
@@ -112,16 +99,16 @@ TEST(image, floats)
     // astoundingly, even this unit test, with a value type which cannot be converted implicitely, would not have made
     // a difference in the cryptic compiler output for the bug which cost me so much time
 
-    const rc::Image<float, 3, 2, 1, 1> image({0.0F, 1.0F, 2.0F, 3.0F, 4.0F, 5.0F}, NAN);
+    const rc::Image<float, 3, 2, 1> image({0.0F, 1.0F, 2.0F, 3.0F, 4.0F, 5.0F}, NAN);
 
     ASSERT_EQ(image.size(), 6);
-    ASSERT_EQ(image.padded_size(), 60);
-    EXPECT_FLOAT_EQ(image(0, 0, 0), 0.0F);
-    EXPECT_FLOAT_EQ(image(0, 1, 0), 1.0F);
-    EXPECT_FLOAT_EQ(image(1, 0, 0), 2.0F);
-    EXPECT_FLOAT_EQ(image(1, 1, 0), 3.0F);
-    EXPECT_FLOAT_EQ(image(2, 0, 0), 4.0F);
-    EXPECT_FLOAT_EQ(image(2, 1, 0), 5.0F);
+    ASSERT_EQ(image.padded_size(), 20);
+    EXPECT_FLOAT_EQ(image(0, 0), 0.0F);
+    EXPECT_FLOAT_EQ(image(0, 1), 1.0F);
+    EXPECT_FLOAT_EQ(image(1, 0), 2.0F);
+    EXPECT_FLOAT_EQ(image(1, 1), 3.0F);
+    EXPECT_FLOAT_EQ(image(2, 0), 4.0F);
+    EXPECT_FLOAT_EQ(image(2, 1), 5.0F);
 }
 
 }
