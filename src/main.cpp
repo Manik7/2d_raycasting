da67b0359d971dbb892f4d4070962f9980b31ffd
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-15 21:53:59 +0200

    remove eigen example from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <cstdlib>
 #include <iostream>
-#include <Eigen/Dense>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/highgui.hpp>
 
@@ -11,17 +10,6 @@
 
 int main()
 {
-    Eigen::Matrix3d a;
-    a << 1, 2, 3,
-         4, 5, 6,
-         7, 8, 9;
-    Eigen::Matrix3d b;
-    b << 2, 3, 1,
-         4, 6, 7,
-         8, 5, 9;
-    std::cout << "a + b =\n" << a + b << std::endl;
-    std::cout << "a - b =\n" << a - b << std::endl;
-
     using namespace rc;
 
     // INPUT
