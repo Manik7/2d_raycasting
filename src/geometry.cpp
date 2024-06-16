1b54d99aa9e8303a374f235acefc8de2ede8d988
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-16 22:14:00 +0200

    define 'Graph' datatype which represents a collection of vertices and edges

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -9,4 +9,46 @@ namespace rc
         return Vertex{x, y, 1.0F};
     }
 
+    Graph coordinate(Transformation const& origin)
+    {
+        // Technically, default constructing BGR (which is black) is incorrect, since a coordinate system does not have
+        // a color. Since it contains no vertices and no edges which can be rendered, and thus nothing to which the
+        // color can be applied, this should not cause any issues. Wrapping the color in a std::optional just for this
+        // one special case would complicate the code everywhere, and seems like overkill.
+        return Graph{origin, {}, {}, BGR{}};
+    }
+
+    Graph point(Transformation const& origin, BGR const& color)
+    {
+        return Graph{origin, {vertex(0.0F, 0.0F)}, {}, color};
+    }
+
+    Graph line(Transformation const& origin, Vertex const& other, BGR const& color)
+    {
+        return Graph{origin, {vertex(0.0F, 0.0F), other}, {std::make_pair(0, 1)}, color};
+    }
+
+    Graph rectangle(Transformation const& origin, Vertex const& other, BGR const& color)
+    {
+        return Graph{origin,
+                     {vertex(0.0F, 0.0F), vertex(other[0], 0.0F), other, vertex(0.0F, other[1])},
+                     {std::make_pair(0, 1), std::make_pair(1, 2), std::make_pair(2, 3), std::make_pair(3, 0)},
+                     color};
+    }
+
+    Graph right_triangle(Transformation const& origin, Vertex const& other, BGR const& color)
+    {
+        return Graph{origin,
+                     {vertex(0.0F, 0.0F), vertex(other[0], 0.0F), other},
+                     {std::make_pair(0, 1), std::make_pair(1, 2), std::make_pair(2, 0)},
+                     color};
+    }
+
+    Graph triangle(Transformation const& origin, Vertex const& a, Vertex const& b, BGR const& color)
+    {
+        return Graph{origin,
+                     {vertex(0.0F, 0.0F), a, b},
+                     {std::make_pair(0, 1), std::make_pair(1, 2), std::make_pair(2, 0)},
+                     color};
+    }
 }
--- src/geometry.hpp
+++ src/geometry.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+#include <utility>
 #include <Eigen/Dense>
 #include "constants.hpp"
 
@@ -8,5 +10,25 @@ namespace rc
     using Vertex         = Eigen::Vector3f;
     using Transformation = Eigen::Matrix3f;
 
+    struct Graph // a bag of vertices and lines
+    {
+        Transformation _transformation{};
+        std::vector<Vertex> _vertices{};
+        std::vector<std::pair<int, int>> _edges{}; // a pair (a, b) means that there is a line from _vertices[a] to _vertices[b]
+        BGR _color{};
+        // for 3d, add one more vector, to encode faces between edges
+    };
+
     Vertex vertex(float x, float y);
+
+    Graph coordinate(Transformation const& origin);
+    Graph point(Transformation const& origin, BGR const& color);
+    Graph line(Transformation const& origin, Vertex const& other, BGR const& color);
+    Graph rectangle(Transformation const& origin, Vertex const& other, BGR const& color);
+    Graph right_triangle(Transformation const& origin, Vertex const& other, BGR const& color);
+    Graph triangle(Transformation const& origin, Vertex const& a, Vertex const& b, BGR const& color);
+
+    // TODO: test free functions to create lines, polylines, triangles, right-triangles, and rectangles
+    // TODO: free function to find the axis-aligned bounding box by finding the min-max values of the points (for each axis)
+    // TODO: (later on in another ticket) free function to intersect a ray with a bag-of-lines
 }
