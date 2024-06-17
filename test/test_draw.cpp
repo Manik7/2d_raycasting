28b58d1dfb01df4612ab4a95a6ff97a8e5141986
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-17 23:52:10 +0200

    WIP: failing line-drawing tests, presumably due to rounding errors

--- test/test_draw.cpp
+++ test/test_draw.cpp
@@ -10,11 +10,110 @@ namespace
 TEST(draw, vertex)
 {
     rc::GenericImage<rc::BGR, 3, 3, 0> image{};
-    rc::draw(rc::vertex(1,1), rc::color::white, image);
-    rc::BGR color = image(1,1);
-    EXPECT_EQ(color._blue, 255U);
-    EXPECT_EQ(color._green, 255U);
-    EXPECT_EQ(color._red, 255U);
+    rc::draw(rc::vertex(1,1), rc::color::green, image);
+
+    EXPECT_EQ(image(0,0)._green, 0U);
+    EXPECT_EQ(image(0,1)._green, 0U);
+    EXPECT_EQ(image(0,2)._green, 0U);
+    EXPECT_EQ(image(1,0)._green, 0U);
+    EXPECT_EQ(image(1,1)._green, 255U);
+    EXPECT_EQ(image(1,2)._green, 0U);
+    EXPECT_EQ(image(2,0)._green, 0U);
+    EXPECT_EQ(image(2,1)._green, 0U);
+    EXPECT_EQ(image(2,2)._green, 0U);
+    for (rc::BGR const& color : image._data)
+    {
+        EXPECT_EQ(color._blue, 0U);
+        EXPECT_EQ(color._red, 0U);
+    }
+}
+
+TEST(draw, point)
+{
+    rc::GenericImage<rc::BGR, 3, 3, 0> image{};
+    rc::Graph const graph = rc::point(rc::translate(1,1), rc::color::green);
+    rc::draw(graph, image);
+
+    EXPECT_EQ(image(0,0)._green, 0U);
+    EXPECT_EQ(image(0,1)._green, 0U);
+    EXPECT_EQ(image(0,2)._green, 0U);
+    EXPECT_EQ(image(1,0)._green, 0U);
+    EXPECT_EQ(image(1,1)._green, 255U);
+    EXPECT_EQ(image(1,2)._green, 0U);
+    EXPECT_EQ(image(2,0)._green, 0U);
+    EXPECT_EQ(image(2,1)._green, 0U);
+    EXPECT_EQ(image(2,2)._green, 0U);
+    for (rc::BGR const& color : image._data)
+    {
+        EXPECT_EQ(color._blue, 0U);
+        EXPECT_EQ(color._red, 0U);
+    }
+}
+
+TEST(draw, line)
+{
+    rc::GenericImage<rc::BGR, 3, 3, 0> image{};
+    rc::Graph const graph = rc::line(rc::translate(0, 0), rc::vertex(0,2), rc::color::green);
+    rc::draw(graph, image);
+
+    EXPECT_EQ(image(0,0)._green, 255U);
+    EXPECT_EQ(image(0,1)._green, 255U);
+    EXPECT_EQ(image(0,2)._green, 255U);
+    EXPECT_EQ(image(1,0)._green, 0U);
+    EXPECT_EQ(image(1,1)._green, 0U);
+    EXPECT_EQ(image(1,2)._green, 0U);
+    EXPECT_EQ(image(2,0)._green, 0U);
+    EXPECT_EQ(image(2,1)._green, 0U);
+    EXPECT_EQ(image(2,2)._green, 0U);
+    for (rc::BGR const& color : image._data)
+    {
+        EXPECT_EQ(color._blue, 0U);
+        EXPECT_EQ(color._red, 0U);
+    }
+}
+
+TEST(draw, translated_line)
+{
+    rc::GenericImage<rc::BGR, 3, 3, 0> image{};
+    rc::Graph const graph = rc::line(rc::translate(1, 1), rc::vertex(0,2), rc::color::green);
+    rc::draw(graph, image);
+
+    EXPECT_EQ(image(0,0)._green, 0U);
+    EXPECT_EQ(image(0,1)._green, 0U);
+    EXPECT_EQ(image(0,2)._green, 0U);
+    EXPECT_EQ(image(1,0)._green, 0U);
+    EXPECT_EQ(image(1,1)._green, 255U);
+    EXPECT_EQ(image(1,2)._green, 255U);
+    EXPECT_EQ(image(2,0)._green, 0U);
+    EXPECT_EQ(image(2,1)._green, 0U);
+    EXPECT_EQ(image(2,2)._green, 0U);
+    for (rc::BGR const& color : image._data)
+    {
+        EXPECT_EQ(color._blue, 0U);
+        EXPECT_EQ(color._red, 0U);
+    }
+}
+
+TEST(draw, rotated_line)
+{
+    rc::GenericImage<rc::BGR, 3, 3, 0> image{};
+    rc::Graph const graph = rc::line(rc::rotate(rc::Degrees(-90)), rc::vertex(0,2), rc::color::green);
+    rc::draw(graph, image);
+
+    EXPECT_EQ(image(0,0)._green, 0U);
+    EXPECT_EQ(image(0,1)._green, 0U);
+    EXPECT_EQ(image(0,2)._green, 0U);
+    EXPECT_EQ(image(1,0)._green, 0U);
+    EXPECT_EQ(image(1,1)._green, 0U);
+    EXPECT_EQ(image(1,2)._green, 0U);
+    EXPECT_EQ(image(2,0)._green, 255U);
+    EXPECT_EQ(image(2,1)._green, 255U);
+    EXPECT_EQ(image(2,2)._green, 255U);
+    for (rc::BGR const& color : image._data)
+    {
+        EXPECT_EQ(color._blue, 0U);
+        EXPECT_EQ(color._red, 0U);
+    }
 }
 
 }
