dd12abcf31d1c0c82b98cffebed83dd075203d1b
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-17 22:15:57 +0200

    replace buggy line-drawing algorithm with one which works but is incredibly inefficient

--- src/draw.hpp
+++ src/draw.hpp
@@ -16,17 +16,21 @@ namespace rc
     template <typename T, int Height, int Width, int Padding>
     void draw(Vertex const& from, Vertex const& to, T const& value, GenericImage<T, Height, Width, Padding>& image)
     {
-        // naive line drawing algorithm from: https://en.wikipedia.org/wiki/Line_drawing_algorithm
-        const float x1 = from[0];
-        const float y1 = from[1];
-        const float x2 = to[0];
-        const float y2 = to[1];
-        const float dx = x2 - x1;
-        const float dy = y2 - y1;
+        const float x1              = from[0];
+        const float y1              = from[1];
+        const float x2              = to[0];
+        const float y2              = to[1];
+        const float dx              = x2 - x1;
+        const float dy              = y2 - y1;
+        int const length_squared    = dx * dx + dy * dy;
+        const float samplesPerPixel = 4.0F;
 
-        for(float x = x1; x <= x2; ++x)
+        // TODO: replace this TERRIBLY INEFFICIENT line drawing algorithm with something proper like Bresenham's or
+        //       the Gupta-Sproull line drawing algorithm: https://en.wikipedia.org/wiki/Line_drawing_algorithm
+        for(float t = 0; t*t <= length_squared*samplesPerPixel; ++t)
         {
-            const float y = y1 + dy * (x - x1) / dx;
+            float x = x1 + (t*t/(length_squared*samplesPerPixel))*dx;
+            float y = y1 + (t*t/(length_squared*samplesPerPixel))*dy;
             const int i = static_cast<int>(x);
             const int j = static_cast<int>(y);
             if(image.isPixelInBounds(i, j))
