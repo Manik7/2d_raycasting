cea0e55073d3987d4b3083dad2f565790951b45e
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-22 18:30:08 +0200

    cleanup line drawing code slightly

--- src/draw.hpp
+++ src/draw.hpp
@@ -23,21 +23,22 @@ namespace rc
     template <typename T, int Height, int Width, int Padding>
     void draw(Vertex const& from, Vertex const& to, T const& value, GenericImage<T, Height, Width, Padding>& image)
     {
-        const float origin_row      = from[0];
-        const float origin_col      = from[1];
-        const float other_row       = to[0];
-        const float other_col       = to[1];
-        const float delta_row       = other_row - origin_row;
-        const float delta_col       = other_col - origin_col;
-        int const length_squared    = delta_row * delta_row + delta_col * delta_col;
-        const float samplesPerPixel = 2.0F;
+        const float origin_row          = from[0];
+        const float origin_col          = from[1];
+        const float other_row           = to[0];
+        const float other_col           = to[1];
+        const float delta_row           = other_row - origin_row;
+        const float delta_col           = other_col - origin_col;
+        int const length_squared        = delta_row * delta_row + delta_col * delta_col;
+        const float samplesPerPixel     = 2.0F;
+        const float totalSamplesSquared = length_squared * samplesPerPixel * samplesPerPixel;
 
         // TODO: replace this TERRIBLY INEFFICIENT line drawing algorithm with something proper like Bresenham's or
         //       the Gupta-Sproull line drawing algorithm: https://en.wikipedia.org/wiki/Line_drawing_algorithm
-        for(float t = 0; t*t <= length_squared*samplesPerPixel*samplesPerPixel; t+=1.0F)
+        for(float t = 0; t*t <= totalSamplesSquared; t+=1.0F)
         {
-            const int row = round(origin_row + (t*t/(length_squared*samplesPerPixel*samplesPerPixel))*delta_row);
-            const int col = round(origin_col + (t*t/(length_squared*samplesPerPixel*samplesPerPixel))*delta_col);
+            const int row = round(origin_row + (t*t/(totalSamplesSquared))*delta_row);
+            const int col = round(origin_col + (t*t/(totalSamplesSquared))*delta_col);
             if(image.isPixelInBounds(row, col))
             {
                 image(row, col) = value;
